d228b0a9120c88723cdeabf977fe8fabdb3e026b
cjnbennett <conor.jn.bennett@gmail.com>
2023-08-19 18:00:20 +0800

    added basic routing and pages

---
+++ pages/article.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Welcome to the Article page</h1>
+      <nuxt-link to="/">Go Home</nuxt-link>
+    </div>
+  </template>
---
+++ pages/edit.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Welcome to the Edit page</h1>
+      <nuxt-link to="/">Go Home</nuxt-link>
+    </div>
+  </template>
