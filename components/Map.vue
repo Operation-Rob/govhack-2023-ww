991ac786ccafc397a928b6e406f0c4f384358ba6
cjnbennett <conor.jn.bennett@gmail.com>
2023-08-19 23:46:24 +0800

    title bar

---
+++ components/Map.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+  import mapboxgl from 'mapbox-gl'
+
+  export default {
+    data() {
+      return {
+        access_token: process.env.MAPBOX_TOKEN,
+        map:{}
+      }
+    },
+    mounted() {
+      this.createMap()
+    },
+    methods: {
+      createMap() {
+        mapboxgl.accessToken = this.access_token
+        console.log(mapboxgl.accessToken)
+        this.map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/cjnbennett/clli2gymn00jn01pr0i2s6xne',
+          center: [143.36599431114507,-31.68953090523168],
+          zoom: 6,
+          maxZoom: 6,
+          minZoom: 6,
+          maxBounds: [140.5, -35.159189, 160.976835, -20.980645],
+          dragPan: false,
+          scrollZoom: false,
+          touchZoomRotate: false
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,13 @@
 <template>
-  <div>
-    <div id="map"></div>
-  </div>
+    <div>
+    <TopBar class="min-h-128px"/>
+    <AppMap class="w-full h-[calc(100vh-56px)]"/>
+    </div>
 </template>
 
-<script>
-  import mapboxgl from 'mapbox-gl'
-
-  export default {
-    data() {
-      return {
-        access_token: process.env.MAPBOX_TOKEN,
-        map:{}
-      }
-    },
-    mounted() {
-      this.createMap()
-    },
-    methods: {
-      createMap() {
-        mapboxgl.accessToken = this.access_token
-        console.log(mapboxgl.accessToken)
-        this.map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/cjnbennett/clli2gymn00jn01pr0i2s6xne',
-          center: [143.36599431114507,-31.68953090523168],
-          zoom: 6,
-          maxZoom: 6,
-          minZoom: 6,
-          maxBounds: [140.5, -35.159189, 160.976835, -20.980645],
-          dragPan: false,
-          scrollZoom: false,
-          touchZoomRotate: false
-        })
-      }
-    }
-  }
+<script setup>
+//import AppSidebar from '@/components/Sidebar/AppSidebar.vue'
+import TopBar from '@/components/Bar.vue'
+import AppMap from '@/components/Map.vue'
+//import TopBar from '@/components/TopBar/TopBar.vue'
 </script>
-
-<style scoped>
-#map {
-  width: 100%;
-  height: 100vh;
-}
-</style>
