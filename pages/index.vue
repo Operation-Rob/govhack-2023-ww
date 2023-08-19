7d034903d7a20a81f5453d6e42f663b85e3b578d
cjnbennett <conor.jn.bennett@gmail.com>
2023-08-19 20:17:42 +0800

    added map

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,39 @@
 <template>
-    <div>
-      <h1>Welcome to the Map page</h1>
-      <nuxt-link to="/article">Go to Article</nuxt-link>
-      <nuxt-link to="/edit">Go to Edit</nuxt-link>
-    </div>
-  </template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+  import mapboxgl from 'mapbox-gl'
+
+  export default {
+    data() {
+      return {
+        map:{}
+      }
+    },
+    mounted() {
+      this.createMap()
+    },
+    methods: {
+      createMap() {
+        mapboxgl.accessToken = process.env.MAPBOX_TOKEN
+        console.log(mapboxgl.accessToken)
+        this.map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/satellite-v9',
+          zoom: 11,
+          center: [107.61861, -6.90389]
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
