e6bb9b3e5d6d61fea8d201d94a93d6be8fb2ac57
cjnbennett <conor.jn.bennett@gmail.com>
2023-08-20 15:19:45 +0800

    create article

--- components/Bar.vue
+++ components/Bar.vue
@@ -1,18 +1,44 @@
 <template>
-    <div class="bg-lightpink flex items-center justify-center h-16">
-      <!-- Logo Image -->
-      <div class="logo-container mr-4">
-        <img src="/image/logo.png" alt="Website Logo">
-      </div>
+    <div class="bg-lightpink flex justify-between items-center h-16 p-4"> <!-- Added padding for some space -->
       
-      <!-- Website Title -->
-      <h1 class="font-blackmango text-white">Wisdom Walks</h1>
+      <!-- Left side: Logo and Title -->
+      <div class="flex items-center">
+        <!-- Logo Image -->
+        <div class="logo-container mr-4">
+          <img src="/image/logo.png" alt="Website Logo">
+        </div>
+        
+        <!-- Website Title -->
+        <h1 class="font-blackmango text-white">Wisdom Walks</h1>
+      </div>
+  
+      <!-- Right side: Button -->
+      <button @click="navigateToNew" class="bg-darkpink text-white py-2 px-4 rounded font-blackmango">
+        Create Article
+      </button>
+      
     </div>
   </template>
+
+<script>
+export default {
+  methods: {
+    navigateToNew() {
+      const articleUrl = `/newarticle`;
+      window.location.href = articleUrl;
+    }
+  }
+}
+</script>
+  
   
   <style scoped>
   .bg-lightpink {
       background-color: #f4dbce;
+  }
+
+  .bg-darkpink {
+      background-color: #ec919c;
   }
   
   .font-blackmango {
